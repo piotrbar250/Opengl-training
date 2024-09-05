620ce5fe865894352f98c628e0053bbacdd800ae
piotrbar250 <p.baranowski250@gmail.com>
2024-09-05 22:05:07 +0200

    10 cubes

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 
 #include "Triangle.h"
 #include "Rectangle.h"
+#include "Cube.h"
 
 #include "Textures.h"
 #include "stb_image.h"
@@ -40,6 +41,7 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Couldn't load opengl" << std::endl;
@@ -47,22 +49,40 @@ int main()
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     unsigned int texture1 = textureContainer();
     unsigned int texture2 = textureAwesomeFace();
     
-    Triangle t1;
-    Rectangle r1, r2;
-
+    // Triangle t1;
+    // Rectangle r1, r2;
+    Cube c1;
     mat4 transform = mat4(1.0f);
 
     mat4 model = mat4(1.0f);
     mat4 view = mat4(1.0f);
     mat4 projection = mat4(1.0f);
-    
+
+    view = translate(mat4(1.0f), vec3(0.0f, 0.0f, -3.0f));
+    projection = perspective(radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
+
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // TEXTURES
 
@@ -96,19 +116,28 @@ int main()
         // view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         // projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         
-        model = rotate(mat4(1.0f), -float(glfwGetTime()), vec3(1.0f, 0.0f, 0.0f));
-        model = rotate(mat4(1.0f), radians(-55.0f), vec3(1.0f, 0.0f, 0.0f));
+        // model = rotate(mat4(1.0f), -float(glfwGetTime()), vec3(1.0f, 0.0f, 0.0f));
+        // model = rotate(mat4(1.0f), radians(-55.0f), vec3(1.0f, 0.0f, 0.0f));
 
-        view = translate(mat4(1.0f), vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), float(SCR_WIDTH)/float(SCR_HEIGHT), 0.1f, 100.0f);
+        // view = translate(mat4(1.0f), vec3(0.0f, 0.0f, -3.0f));
+        // projection = glm::perspective(glm::radians(45.0f), float(SCR_WIDTH)/float(SCR_HEIGHT), 0.1f, 100.0f);
 
 
-        r1.shader.use();
-        r1.shader.setMat4("model", model);
-        r1.shader.setMat4("view", view);
-        r1.shader.setMat4("projection", projection);
+        // r1.shader.use();
+        // r1.shader.setMat4("model", model);
+        // r1.shader.setMat4("view", view);
+        // r1.shader.setMat4("projection", projection);
+        // mat4 model = rotate(mat4(1.0f), radians(-55.0f), vec3(1.0f, 0.0f, 0.0f));
+        float tim = (float)glfwGetTime();
+        mat4 model = rotate(mat4(1.0f), (float)glfwGetTime(), vec3(1.0f, 1.0f, 1.0f));
+        c1.draw(view, projection, model);
 
-        r1.draw();
+        // for(int i = 0; i < 10; i++)
+        // {
+        //     mat4 model = translate(mat4(1.0f), cubePositions[i]) * rotate(mat4(1.0f), tim, vec3(1.0f, 1.0f, 1.0f));
+        //     c1.draw(view, projection, model);
+        // }
+
 
         // trans = translate(mat4(1.0f), vec3(-0.5f, 0.5f, 0.0f));
         // mat4 scal = scale(mat4(1.0f), sin((float)glfwGetTime()) * vec3(1.0f, 1.0f, 1.0f)); 
